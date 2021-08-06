fcf7c3b1b382817056bb96bbab1d51ed5256ff5a
Steven <stevenduong1983tx@gmail.com>
2021-08-06 18:37:02 +0700

    add dataTable With checkBox

--- force-app/main/default/aura/ctrlf_MyPropertyResult/ctrlf_MyPropertyResultController.js
+++ force-app/main/default/aura/ctrlf_MyPropertyResult/ctrlf_MyPropertyResultController.js
@@ -2,8 +2,23 @@
 
 	fetchAllProperty : function(component, event, helper) {
 
-	helper.fetchAllPropertyHelper(component, event, helper);
+	helper.fetchAllPropertyHelper(component, event, helper);	
+	},
+	ownerDetailsClick : function(component, event, helper){
+		var eventSource = event.getSource();
+		var proOwnerId = eventSource.get('v.value');
+		component.set('v.proOwnerId',proOwnerId);
+		component.set('v.openOwnerDetails',true);
+	},
+	closeOwnerModal : function(component, event, helper){
+		component.set('v.openOwnerDetails',false);
+	},
+	feedbackClicked : function(component, event, helper){
 
-}
+	},
+	NavigateToProDetails : function(component, event, helper){
+
+	}
+
 
 })
